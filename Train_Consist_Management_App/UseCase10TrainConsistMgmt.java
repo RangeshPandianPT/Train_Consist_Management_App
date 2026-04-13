681118f25d4039546d86b7898951ad352d411f13
RangeshPandianPT <rangeshpandian@gmail.com>
2026-04-13 22:18:49 +0530

    UC10: Count Total Seats in Train (reduce)

---
+++ Train_Consist_Management_App/UseCase10TrainConsistMgmt.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class UseCase10TrainConsistMgmt {
+
+    // Bogie class
+    static class Bogie {
+        String name;
+        int capacity;
+
+        public Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
+
+        public int getCapacity() {
+            return capacity;
+        }
+
+        @Override
+        public String toString() {
+            return name + " -> " + capacity;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("UC10 - Count Total Seats in Train");
+        System.out.println("=====================================\n");
+
+        // Create list of bogies
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("Sleeper", 70));
+
+        // Display bogies
+        System.out.println("Bogies in Train:");
+        for (Bogie b : bogies) {
+            System.out.println(b);
+        }
+
+        // STREAM AGGREGATION USING MAP + REDUCE
+        int totalSeats = bogies.stream()
+                .map(b -> b.getCapacity())     // extract capacity
+                .reduce(0, Integer::sum);      // sum all values
+
+        // Display result
+        System.out.println("\nTotal Seating Capacity of Train: " + totalSeats);
+
+        System.out.println("\nUC10 aggregation completed...");
+    }
+}
